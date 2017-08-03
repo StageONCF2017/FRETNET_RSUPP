ec37f3bf4929ce7a9aa096270552befc0b511bdc
Hamza EL RHAZI <h.elrhazi@enim.ac.ma>
2017-08-03 12:19:26 +0200

    realisation initial du back-end page accueil

--- FRETNET_RSUPP/webFormAccueil.aspx.cs
+++ FRETNET_RSUPP/webFormAccueil.aspx.cs
@@ -7,16 +7,35 @@ using System.Web.UI.WebControls;
 
 namespace FRETNET_RSUPP
 {
+	
 	public partial class accueil : System.Web.UI.Page
 	{
+	//Declaration des variables qui doivent mémoriser les informations d'ouverture
+		public static String moisOuverture;
+		public static String anneeOuverture;
+
+	//Methode StockerMoisAnneeeOuverture pour stocker le mois et l'année séléctionnées 
+	//dans le formulaire d'ouverture dans les variables static moisOuverture et anneeOuverture
+		public void StockerMoisAnneeOuverture()
+		{
+			moisOuverture = LstBxMois.SelectedValue;
+			anneeOuverture = LstBxAnnee.SelectedValue;
+			LblEtatOuverture.ForeColor = System.Drawing.Color.Green;
+			LblEtatOuverture.Text = " L'état d'ouverture : Le mois "+ moisOuverture+ " de l'année " + anneeOuverture + " est en état d'ouverture";
+	
+		}
 		protected void Page_Load(object sender, EventArgs e)
 		{
-
+		//Initialiser l'état d'ouverture en indiquant que aucun mois n'est en état d'ouverture
+			LblEtatOuverture.ForeColor = System.Drawing.Color.Red;
+			LblEtatOuverture.Text = " L'état d'ouverture : Aucun mois n'est en ouverture";
 		}
 
-        protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
+		//Evennement clique sur le bouton d'ouverture d'un mois
+		//Le clique sur ce bouton active le stockage
+		protected void BtnMoisAnneeOuverture_Click(object sender, EventArgs e)
+		{
+			StockerMoisAnneeOuverture();
+		}
 	}
 }
